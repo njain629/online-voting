bcfe6d53c45e986f851d5edb513a17640fe853a7
njain629 <njain629@gmail.com>
2018-01-23 18:37:33 +0530

    access denied

---
+++ admin/access-denied.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Simple PHP Polling System Access Denied</title>
+		<link href="css/admin_styles.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body bgcolor="tan">
+		<center><b><font color = "brown" size="6">Simple PHP Polling System</font></b></center><br><br>
+			<div id="page">
+				<div id="header">
+			  		<h1>Access Denied</h1>
+			  			<a href="admin.php">Home</a> | <a href="manage-admins.php">Manage Administrators</a> | <a href="positions.php">Manage Positions</a> | <a href="candidates.php">Manage Candidates</a> | <a href="results.php">Poll Results</a>
+				</div>
+				<div id="container">
+					<div class="err">Access Denied!</div>
+			  			<p>You don't have access to this resource. <a href="index.php">Click here</a> to login first.</p>
+				</div>
+				<div id="footer">
+			  		<div class="bottom_addr">&copy; 2017 Simple PHP Polling System. All Rights Reserved</div>
+				</div>
+			</div>
+	</body>
+</html>
